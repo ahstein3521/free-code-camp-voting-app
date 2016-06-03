d044c9f059324fbf814579bbc5e2239dd1466934
ahs3521 <adamhs3521@gmail.com>
2016-06-03 12:38:36 -0400

    Update jwt.strategy.js

--- server/config/strategies/jwt.strategy.js
+++ server/config/strategies/jwt.strategy.js
@@ -2,17 +2,15 @@ const User = require('../../models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
-// Setup options for JWT Strategy
+
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: require('../../secret')
 };
 
-// Create JWT strategy
+
 module.exports = new JwtStrategy(jwtOptions, function(payload, done) {
-  // See if the user ID in the payload exists in our database
-  // If it does, call 'done' with that other
-  // otherwise, call done without a user object
+
   User.findById(payload.sub, function(err, user) {
     if (err) { return done(err, false); }
 
