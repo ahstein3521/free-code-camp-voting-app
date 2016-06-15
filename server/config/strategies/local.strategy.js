83ee2c7686a1f20f1af93cf3e41efa8f4856b4ce
ahs3521 <adamhs3521@gmail.com>
2016-06-15 13:42:47 -0400

    added credits

--- server/config/strategies/local.strategy.js
+++ server/config/strategies/local.strategy.js
@@ -21,3 +21,15 @@ module.exports = new LocalStrategy(localOptions, function(email, password, done)
     });
   });
 });
+/*
+A great portion of this code comes directly from Stephen Grider's redux course on Udemy. 
+-- https://www.udemy.com/react-redux-tutorial/learn/v4/content --
+I highly reccomend checking out his youtube channel 'rally coding' as well.
+
+This file is from a lesson of his on user authorization middleware, it required zero refactoring for the 
+purposes of this project, and has been monumental in helping cement my understanding of hiw to apply middleware to code.
+The last thing I would want to do is take credit for the work of this excellent instructor 
+especially when his tutorials helped me establish a solid foundation for creating full scale apps with the MERN
+stack.
+
+*/
